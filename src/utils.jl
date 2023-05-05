71e6f1302fec759a182f9b44a12a32f80eb6b285
Sam Morgenstern <stm16109@mit.edu>
2023-05-04 21:15:08 -0400

    update project

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,11 @@
 # Parameter Setup
+module utils
+
+import RHSfunc
+export setup_CH,makesparseprob,makegif
+
+using Symbolics,SparseDiffTools,SparseArrays,LinearAlgebra,CUDA,PreallocationTools,Plots,DifferentialEquations
+
 function setup_CH(ψ; gpuflag = false,kw...)
     #Set up the simulation domain from the mask
     Nx, Ny = size(ψ) # grab size
@@ -89,4 +96,6 @@ function makegif(fullsol::CHsol; fpsv=10)
     masksol = Array(fullsol.sol).*fullsol.ψ_binary;
     anim = makegif(masksol)
     return gif(anim, fps = fpsv)
+end
+
 end
